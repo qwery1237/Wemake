f853f3c65391408eb74acfd4a602d6b43a6ed566
qwery1237 <94043757+qwery1237@users.noreply.github.com>
2025-02-26 15:12:46 -0800

    start navigation bar

--- app/common/pages/home.tsx
+++ app/common/pages/home.tsx
@@ -2,7 +2,7 @@ import { Button } from '~/common/components/ui/button';
 
 export default function HomePage() {
   return (
-    <main className='container mx-auto px-4 py-8'>
+    <main className='container mx-auto px-4 py-8 w-full bg-white'>
       <Button>Get Started</Button>
     </main>
   );
